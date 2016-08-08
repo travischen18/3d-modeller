06a47d82ca35d64ff6f94a524c532d533f15c883
Travis Chen <tchen96@cs.washington.edu>
2016-08-08 11:28:12 -0700

    Refactored plane

--- app/src/main/java/com/example/travis/a3dmodeller/objects/Plane.java
+++ app/src/main/java/com/example/travis/a3dmodeller/objects/Plane.java
@@ -3,6 +3,7 @@ package com.example.travis.a3dmodeller.objects;
 import com.example.travis.a3dmodeller.Constants;
 import com.example.travis.a3dmodeller.data.VertexArray;
 import com.example.travis.a3dmodeller.programs.ColorShaderProgram;
+import com.example.travis.a3dmodeller.util.Geometry;
 
 import static android.opengl.GLES10.*;
 
@@ -10,36 +11,44 @@ import static android.opengl.GLES10.*;
  * Created by Travis on 8/5/16.
  */
 public class Plane {
-    private static final int POSITION_COMPONENT_COUNT = 3;
-    private static final int STRIDE = (POSITION_COMPONENT_COUNT) * Constants.BYTES_PER_FLOAT;
+    private static final int NUM_VERTICES = 4;
+    private static final int FLOATS_PER_VERTEX = 3;
+    private static final int STRIDE = (FLOATS_PER_VERTEX) * Constants.BYTES_PER_FLOAT;
 
-    private static final float[] VERTEX_DATA = {
-            // Order of coordinates: X, Y, Z
-
-            // Triangle Fan
-            0.5f, 0f, -0.8f,
-            -0.5f, 0f, 0.8f,
-            0.5f, 0f, 0.8f,
-            0.5f,  0f, -0.8f,
-            -0.5f, 0f, -0.8f,
-            -0.5f, 0f, 0.8f};
+    private float[] VERTEX_DATA;
 
     private final VertexArray vertexArray;
 
-    public Plane() {
+    // Planes are always initialized flat on the x-z plane
+    public Plane(Geometry.Point center, float x_length, float z_length) {
+        buildVertexData(center, x_length / 2, z_length / 2);
         vertexArray = new VertexArray(VERTEX_DATA);
+    }
+
+    private void buildVertexData(Geometry.Point center, float x_factor, float z_factor) {
+        float x_center = center.getX();
+        float y_center = center.getY();
+        float z_center = center.getZ();
+
+        VERTEX_DATA = new float[]{
+                x_center + x_factor, y_center, z_center + z_factor,
+                x_center + x_factor, y_center, z_center - z_factor,
+                x_center - x_factor, y_center, z_center + z_factor,
+                x_center - x_factor, y_center, z_center - z_factor,
+        };
+
     }
 
     public void bindData(ColorShaderProgram colorProgram) {
         vertexArray.setVertexAttribPointer(
                 0,
                 colorProgram.getPositionAttributeLocation(),
-                POSITION_COMPONENT_COUNT,
+                FLOATS_PER_VERTEX,
                 STRIDE);
     }
 
     public void draw() {
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, 12);
     }
 
 }
--- app/src/main/java/com/example/travis/a3dmodeller/util/ObjectBuilder.java
+++ app/src/main/java/com/example/travis/a3dmodeller/util/ObjectBuilder.java
@@ -1,60 +1,61 @@
-package com.example.travis.a3dmodeller.util;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static android.opengl.GLES20.*;
-
-/**
- * Created by Travis on 8/7/16.
- */
-public class ObjectBuilder {
-
-    private final int FLOATS_PER_VERTEX = 3;
-    private final float[] vertexData;
-    private final List<DrawCommand> drawCommandList = new ArrayList<DrawCommand>();
-    private int offset = 0;
-
-    static class ObjectData {
-        final float[] vertexData;
-        final List<DrawCommand> drawCommandList;
-
-        ObjectData(float[] vertexData, List<DrawCommand> drawCommandList) {
-            this.vertexData = vertexData;
-            this.drawCommandList = drawCommandList;
-        }
-    }
-
-    static interface DrawCommand {
-        void draw();
-    }
-
-    private ObjectBuilder(int numVertices) {
-        vertexData = new float[numVertices * FLOATS_PER_VERTEX];
-    }
-
-    // Creates a plane using the center point
-    // and the xlength (edge to edge) and zlength (edge to edge)
-    public static ObjectData createPlane(float x, float y, float z, float xlength, float zlength) {
-
-    }
-
-    private void addPlane() {
-
-        final int NUM_VERTICES = 4;
-        int startVertex = offset / FLOATS_PER_VERTEX;
-
-
-
-        vertexData[]
-
-        drawCommandList.add(new DrawCommand() {
-            @Override
-            public void draw() {
-                glDrawArrays(GL_TRIANGLE_STRIP, 0, NUM_VERTICES);
-            }
-        });
-    }
-
-
-}
+//package com.example.travis.a3dmodeller.util;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import static android.opengl.GLES20.*;
+//
+///**
+// * Created by Travis on 8/7/16.
+// */
+//public class ObjectBuilder {
+//
+//    private final int FLOATS_PER_VERTEX = 3;
+//    private final float[] vertexData;
+//    private final List<DrawCommand> drawCommandList = new ArrayList<DrawCommand>();
+//    private int offset = 0;
+//
+//    static class ObjectData {
+//        final float[] vertexData;
+//        final List<DrawCommand> drawCommandList;
+//
+//        ObjectData(float[] vertexData, List<DrawCommand> drawCommandList) {
+//            this.vertexData = vertexData;
+//            this.drawCommandList = drawCommandList;
+//        }
+//    }
+//
+//    static interface DrawCommand {
+//        void draw();
+//    }
+//
+//    private ObjectBuilder(int numVertices) {
+//        vertexData = new float[numVertices * FLOATS_PER_VERTEX];
+//    }
+//
+//    // Creates a plane using the center point
+//    // and the xlength (edge to edge) and zlength (edge to edge)
+//    public static ObjectData createPlane(float center_x, float center_y, float center_z, float xlength, float zlength) {
+//
+//    }
+//
+//    // Private method to construct a plane
+//    private void addPlane(float center_x, float center_y, float center_z, float xlength, float zlength) {
+//
+//        final int NUM_VERTICES = 4;
+//        int startVertex = offset / FLOATS_PER_VERTEX;
+//
+//        vertexData[offset++]
+//
+//        vertexData[]
+//
+//        drawCommandList.add(new DrawCommand() {
+//            @Override
+//            public void draw() {
+//                glDrawArrays(GL_TRIANGLE_STRIP, 0, NUM_VERTICES);
+//            }
+//        });
+//    }
+//
+//
+//}
